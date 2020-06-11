51d659c1d5ab867a307d93b6caa09f407f6cbe70
bharath <bharath@site.com>
2020-06-11 17:06:20 +0100

    Need to add the Content

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,9 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    Author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    Title = models.CharField(max_length=200)
-    Text = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
@@ -15,4 +15,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.Title
+        return self.title
