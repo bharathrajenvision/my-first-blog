e8936f08f8b21d3822995ef59c7c9612d45c7192
bharath <bharath@site.com>
2020-06-12 14:51:13 +0100

    no blog_post problem fixed

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from django.utils import timezone
 from .models import Post
+from django.utils import timezone
+
 
 
 # Create your views here.
 
 def post_list(request):
-	posts = Post.objects.filter(title__contains='My')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
  
