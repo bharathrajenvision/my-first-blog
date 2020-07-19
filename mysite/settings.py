9b130595bdcc290aeebbf4cdfd115496f6664358
bharath <bharath@site.com>
2020-07-19 13:12:03 +0100

    Redevopling app

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'blog',
-        'USER': 'brr945',
-        'PASSWORD': 'W0rk1ngTressure*',
-        'HOST': 'brr945.mysql.pythonanywhere-services.com',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
